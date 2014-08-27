051482bf994dbc571fee210c40cf53de720ab19e
Timothy Pfafman <tim@pfafman.com>
2014-08-27 14:33:32 -0700

    for 0.9

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe({
-  summary: "Plivo telephony framework wrapper for Meteor"
+  summary: "Plivo telephony framework wrapper for Meteor",
+  version: "1.0.3",
+  git: "http://github.com/pfafman/meteor-plivo.git"
 });
 
 Npm.depends({ "plivo": "0.1.0" });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.add_files('plivo_npm.js', 'server');
   if(api.export)
     api.export('Plivo');
